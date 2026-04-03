540152cbf7a1ac8ba1d7acaca1ff0a1dc6122682
aditipatel <pate1452@purdue.edu>
2026-04-03 15:48:04 -0500

    made loading of images in cards smoother

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,14 @@
 import '../styles/card.css'
+import { useState } from 'react'
 
 
 function Card({ character, handleCardClick }) {
+    const [isLoaded, setIsLoaded] = useState(false)
+
     return (
         <>
             <button key={character.name} className='card-container' onClick={() => handleCardClick(character.name)}>
-                <img className='character-image' src={character.image} alt="Portrait of" />
+                <img className={isLoaded ? 'loaded' : ''} onLoad={() => setIsLoaded(true)} src={character.image} alt="Portrait of" />
                 <h2 className='character-name'>{character.name}</h2>
             </button>
         </>
