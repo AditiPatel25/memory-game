5ae20aab5df1962b8fdff71cfc3184b16049e847
aditipatel <pate1452@purdue.edu>
2026-04-03 14:28:00 -0500

    added memory game functionality - cards shuffle when unclicked card is clicked - updates current and best score accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,52 @@ const characters =
     'Raboot', 'Ponyta', 'Ninetales', 'Charmeleon',
     'Quilava', 'Magmortar', 'Darumaka', 'Braixen']
 
+
 function App() {
-  const [cardInfo, setCardInfo] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [clickedCards, setClickedCards] = useState(new Set());
 
   useEffect(() => {
     const fetchPokemon = async () => {
       const results = await Promise.all(characters.map(name => fetch('https://pokeapi.co/api/v2/pokemon/' + name)));
       const jsonResults = await Promise.all(results.map(result => result.json()));
       const finalInfo = jsonResults.map(character => ({ name: character.name, image: character.sprites.other['official-artwork'].front_default }))
-      setCardInfo(finalInfo)
+      setCards(finalInfo)
     }
     fetchPokemon()
   }, []);
+
+  function shuffleCards() {
+    let cardsCopy = [...cards];
+    for (let i = cardsCopy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      [cardsCopy[i], cardsCopy[j]] = [cardsCopy[j], cardsCopy[i]];
+    }
+    setCards(cardsCopy)
+  }
+
+  function updateScore(name) {
+    console.log('clicked:', name)
+    console.log('has name:', clickedCards.has(name))
+    if (clickedCards.has(name)) {
+      setClickedCards(new Set());
+      setCurrentScore(0);
+      if (currentScore > bestScore) {
+        setBestScore(currentScore)
+      }
+    } else {
+      setClickedCards(new Set([...clickedCards, name]))
+      setCurrentScore(currentScore + 1)
+      if (clickedCards.size + 1 === characters.length) {
+        setBestScore(currentScore + 1)
+        setCurrentScore(0)
+      }
+      shuffleCards()
+    }
+  }
 
   return (
     <>
@@ -27,14 +61,14 @@ function App() {
         <h1 className='header-text'>Memory Game</h1>
         <h3>Click every Pokémon once — but watch out, they shuffle after each pick! Repeat a Pokémon and it's game over <Frown size={18} /></h3>
         <div className='scoreboard'>
-          <p className='scoreboard-text'>Current Score: 12</p>
+          <p className='scoreboard-text'>Current Score: {currentScore}</p>
           <div className='divider'></div>
-          <p className='scoreboard-text'>Best Score <Trophy size={16} /> : 4 </p>
+          <p className='scoreboard-text'>Best Score <Trophy size={16} /> : {bestScore} </p>
         </div>
       </nav>
       <section className='main-section'>
-        {cardInfo.map(character => (
-          <Card key={character.name} character={character} />
+        {cards.map(character => (
+          <Card key={character.name} character={character} updateScore={updateScore} />
         ))}
       </section>
     </>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
 import '../styles/card.css'
 
 
-function Card({ character }) {
+function Card({ character, updateScore }) {
     return (
         <>
-            <button key={character.name} className='card-container'>
+            <button key={character.name} className='card-container' onClick={() => updateScore(character.name)}>
                 <img className='character-image' src={character.image} alt="Portrait of" />
                 <h2 className='character-name'>{character.name}</h2>
             </button>
